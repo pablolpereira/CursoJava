ab5b651b033f5a0b31a6503cd6f74754b6ef5cee
Pablo Luiz Pereira <ppereira@unimedsc.net>
2018-07-24 13:42:51 -0300

    Aula Comentarios e variaveis

---
+++ CursoJava/src/fundamentos/Comentarios.java
@@ -0,0 +1,26 @@
+package fundamentos;
+/**
+ * os comentarios com o @ é conhecido como metadados
+ * @author ppereira (Pablo Luiz Pereira)
+ * @version 1.0
+ * @since 2.0
+ *
+ */
+public class Comentarios {
+	
+	/**
+	 * Esse método é a porta de entrda de um programa java
+	 * 
+	 * @param args parâmetros passados por linha de comando
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+		/*Comentario de multiplas linhas*/
+		System.out.println("Primeiro");
+		
+		//Cuidado com os comentários - Comentario de uma Unica linha
+		System.out.println("Programa");
+	}
+
+}
---
+++ CursoJava/src/fundamentos/VariaveisEConstantes.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class VariaveisEConstantes {
+	public static void main(String[] args) {
+		double raio = 4.5;
+		final double PI = 3.1416;
+		double area = PI * raio * raio;
+		
+		System.out.println("Area é " + area + " m2.");
+		
+		//Usando o printf o valor f é substituida pela variavel area
+		System.out.printf("Area %f é m2. \n" , area);
+		
+		//o valor 2f arredando o valor decimal
+		System.out.printf("Area %.2f é m2. " , area);
+	}
+
+}
