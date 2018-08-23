6b2ada6990d2bc0d9d3791ab9d21068c7e189c49
Pablo Luiz Pereira <ppereira@unimedsc.net>
2018-08-23 13:34:28 -0300

    Continuacao do curso java

---
+++ CursoJava/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class NotacaoPonto {
+	
+	public static void main(String[] args) {
+		String s = "Bom dia? ".toUpperCase().replace("?", " Bia").concat("!");
+		System.out.println(s);
+		
+		
+	}
+
+}
